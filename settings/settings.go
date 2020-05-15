f32de7cbe41fafceb5e043f727a7cf5ce47ec8de
Christiaan Burchell <cjburchell@yahoo.com>
2020-05-15 19:04:17 -0400

    Updated settings dependency

--- settings/settings.go
+++ settings/settings.go
@@ -8,27 +8,27 @@ import (
 // Get the pub sub settings
 func Get(settings settings.ISettings) pubsub.Settings {
 	return pubsub.Settings{
-		Provider: pubsub.ProviderType(settings.Get("PubSubProvider", string(pubsub.MemoryProvider))),
-		Google:   getGoogleSettings(settings),
-		Nats:     getNatsSettings(settings),
+		Provider: pubsub.ProviderType(settings.Get("Provider", string(pubsub.MemoryProvider))),
+		Google:   getGoogleSettings(settings.GetSection("Google")),
+		Nats:     getNatsSettings(settings.GetSection("Nats")),
 	}
 }
 
 func getNatsSettings(settings settings.ISettings) pubsub.NatsProviderSettings {
 	return pubsub.NatsProviderSettings{
-		URL:      settings.Get("pubSubNatsUrl", "tcp://nats:4222"),
-		Token:    settings.Get("pubSubNatsToken", "token"),
-		User:     settings.Get("pubSubNatsUser", "admin"),
-		Password: settings.Get("pubSubNatsPassword", "password"),
+		URL:      settings.Get("Url", "tcp://nats:4222"),
+		Token:    settings.Get("Token", "token"),
+		User:     settings.Get("User", "admin"),
+		Password: settings.Get("Password", "password"),
 	}
 }
 
 func getGoogleSettings(settings settings.ISettings) pubsub.GoogleProviderSettings {
 	return pubsub.GoogleProviderSettings{
-		ProjectID:          settings.Get("googlePubSubProjectId", ""),
-		CredentialsFile:    settings.Get("googlePubSubCredentialsFile", ""),
-		SubscriptionSuffix: settings.Get("googlePubSubscriptionSuffix", ""),
-		CreateTopic:        settings.GetBool("googlePubSubCreateTopic", false),
-		CreateSubscription: settings.GetBool("googlePubSubCreateSubscription", false),
+		ProjectID:          settings.Get("ProjectId", ""),
+		CredentialsFile:    settings.Get("CredentialsFile", ""),
+		SubscriptionSuffix: settings.Get("SubscriptionSuffix", ""),
+		CreateTopic:        settings.GetBool("CreateTopic", false),
+		CreateSubscription: settings.GetBool("CreateSubscription", false),
 	}
 }
